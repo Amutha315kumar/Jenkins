ab19e795cce251e865e00c2dd5164681bb2dfe98
admin <admin@Amutha-Laptop>
2024-11-06 19:38:11 +0530

    2nd commit

--- src/test/java/sample/SimpleCode.java
+++ src/test/java/sample/SimpleCode.java
@@ -1,10 +1,17 @@
 package sample;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SimpleCode {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.out.println("Sample Code");
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(5000);
+		driver.close();
 
 	}
 
